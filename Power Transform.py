446a8a42c540af22ecde232f74722459eeb0468d
Muhammad Sulaiman <muhammad.sulaiman@emumba.com>
2025-01-27 20:41:00 +0500

    Add getRandomImage function

--- Power Transform.py
+++ Power Transform.py
@@ -1,8 +1,11 @@
 import cv2
 import matplotlib.pylab as plt
 from skimage import img_as_float
+import os
+import random
 
-def transform(img, title=''):
+def transform(imgPath, title=''):
+    img=cv2.imread(imgPath)
     if (title == 'inverse'):        
         (thresh, binary) = cv2.threshold(img,  90,  255, cv2.THRESH_BINARY)
         binary1 = 255 - binary        
@@ -19,12 +22,29 @@ def transform(img, title=''):
         plt.subplot(122), plt.imshow(im2), plt.title('Power', size=10)    
         plt.show() 
 
+def imagePathCheck(imagePath):
+    return imagePath.lower().endswith(('.png', '.jpg', '.jpeg', '.gif'))
+
+def getRandomImage(path):
+    if path=="":
+        path="."
+    try:
+        files = os.listdir(path)
+        images = [file for file in files if imagePathCheck(file)]
+        if not images:
+            print("No images found in the specified folder.")
+            return None
+        return random.choice(images)
+    except Exception as e:
+        print(f"An error occurred while selecting image randomly: {e}")
+        return None
+
 def main():    
-    img_file=input("Enter path to an image: ")
-    if img_file=="":
-        img_file="*.{jpg,png}"
-    img = (cv2.imread(img_file))
-    transform(img, title='power')
+    imgFile=input("Enter path to an image: ")
+    if not imagePathCheck(imgFile):
+        print("You did not enter an image, so a random image will be selected from the given directory.\n")
+        imgFile=getRandomImage(imgFile)
+    transform(imgFile,title='power')
 
 if __name__=='__main__':
     main()
